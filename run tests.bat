e0e440b7a1714b10d104df50331cdf6b39eaa85c
victoryamasato <77461762+victoryamasato@users.noreply.github.com>
2023-07-17 01:47:05 -0300

    Update run tests.bat

--- run tests.bat
+++ run tests.bat
@@ -1,3 +1,3 @@
 @echo off
 cd "C:\StoriChallenge"
-cmd /k python -m pytest --browser "chrome" src/TestCases -s --html="HTMLreport.html"
+cmd /k python -m pytest --browser "chrome" src/TestCases --html="HTMLreport.html"
